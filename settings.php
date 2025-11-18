b07bb0b9dae7969b83c143320edc75166e6fd156
VietPham1909 <106209450@student.swin.edu.au>
2025-11-18 15:14:13 +0700

    Add files via upload

--- settings.php
+++ settings.php
@@ -1,8 +1,12 @@
 <?php
-// settings.php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "cars_db"; // Replace with your actual DB name
 
-$host   = "localhost";       
-$user   = "root";            
-$pwd    = "";               
-$sql_db = "exhibition_db";   
+$conn = mysqli_connect($host, $username, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
